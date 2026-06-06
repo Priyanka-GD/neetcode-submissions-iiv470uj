91c0d29dd67c1674302532e67d6cd392e8a2728b
Priyanka Ghosh Dastidar <priyanka.dastidar@gmail.com>
2026-06-06 14:53:26 -0700

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        int left = 0, right = 1;
+        
+        while(right < prices.length) {
+            if(prices[left] < prices[right]){
+                maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+            }else{
+                left = right;
+            }
+            right++;
+        }
+        return maxProfit;
+    }
+}
